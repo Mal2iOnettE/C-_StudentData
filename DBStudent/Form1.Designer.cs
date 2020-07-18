9c0e8ab9772cfe40761fda44e99f6bf6c873759e
Mal2iOnettE <59813928+Mal2iOnettE@users.noreply.github.com>
2020-07-18 14:35:45 +0700

    Add files via upload

---
+++ DBStudent/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace DBStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.stuID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Gpa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.stuName = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.dBStudentsDataSet = new DBStudent.DBStudentsDataSet();
+            this.majorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.majorTableAdapter = new DBStudent.DBStudentsDataSetTableAdapters.MajorTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBStudentsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.majorBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stuID
+            // 
+            this.stuID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stuID.Location = new System.Drawing.Point(384, 85);
+            this.stuID.Name = "stuID";
+            this.stuID.Size = new System.Drawing.Size(296, 34);
+            this.stuID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student ID: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GPA:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(187, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Major:";
+            // 
+            // Gpa
+            // 
+            this.Gpa.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gpa.Location = new System.Drawing.Point(384, 238);
+            this.Gpa.Name = "Gpa";
+            this.Gpa.Size = new System.Drawing.Size(296, 34);
+            this.Gpa.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(187, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Student Name: ";
+            // 
+            // stuName
+            // 
+            this.stuName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stuName.Location = new System.Drawing.Point(384, 134);
+            this.stuName.Name = "stuName";
+            this.stuName.Size = new System.Drawing.Size(296, 34);
+            this.stuName.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.majorBindingSource;
+            this.comboBox1.DisplayMember = "MNAME";
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(384, 186);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(296, 37);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(319, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Student Database";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(90, 373);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(683, 283);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(209, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 47);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(453, 303);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(173, 47);
+            this.clearBtn.TabIndex = 14;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // dBStudentsDataSet
+            // 
+            this.dBStudentsDataSet.DataSetName = "DBStudentsDataSet";
+            this.dBStudentsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // majorBindingSource
+            // 
+            this.majorBindingSource.DataMember = "Major";
+            this.majorBindingSource.DataSource = this.dBStudentsDataSet;
+            // 
+            // majorTableAdapter
+            // 
+            this.majorTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 668);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.stuName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Gpa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stuID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dBStudentsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.majorBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox stuID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Gpa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox stuName;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clearBtn;
+        private DBStudentsDataSet dBStudentsDataSet;
+        private System.Windows.Forms.BindingSource majorBindingSource;
+        private DBStudentsDataSetTableAdapters.MajorTableAdapter majorTableAdapter;
+    }
+}
+
---
+++ DBStudent/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DBStudent
+{
+    public partial class Form1 : Form
+
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+           /* OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, conn);
+            // creating a DataSet object
+            DataSet ds = new DataSet();
+            // filling table Order  
+            dataAdapter.Fill(ds, "DataSudent");
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;*/
+        }
+    
+
+        OleDbDataAdapter DA = new OleDbDataAdapter();
+        DataSet ds = new DataSet();
+        OleDbCommandBuilder comb = new OleDbCommandBuilder();
+        OleDbCommand comm = new OleDbCommand();
+
+        
+        //String sql = "select * from DataSudent";
+        OleDbConnection conn = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\\Users\\Attachai khum-ai\\Desktop\\WORK CODE\\C#\\DBStudent\\DBStudent\\DBStudents.mdb");
+       
+         private void Form1_Load(object sender, EventArgs e)
+         {
+            String sql = "select * from DataSudent";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, conn);
+
+            //DataSet ds = new DataSet();
+            // filling table Order  
+            dataAdapter.Fill(ds, "DataSudent");
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            
+            this.majorTableAdapter.Fill(this.dBStudentsDataSet.Major);
+            try
+            {  
+                     conn.Open();
+                    // ShowData();
+
+
+                 }
+                 catch (Exception ex)
+                 {
+                     MessageBox.Show("Error: Failed to retrieve the required data from the Database.\n{0}", ex.Message);
+                     return;
+                 }
+             }  
+        
+
+         private void button1_Click(object sender, EventArgs e)
+         {
+
+             string sql, s;
+
+             sql = "Select MID FROM Major WHERE MNAME = '" + comboBox1.Text + "'";
+             comm = new OleDbCommand(sql, conn);
+             s = comm.ExecuteScalar().ToString();
+
+             sql = "insert into DataSudent(SID, SNAME, MID, GPA)";
+             sql += "VALUES(";
+             sql += stuID.Text + ",'";
+             sql += stuName.Text + "','";
+             sql += s + "',";
+             sql += Gpa.Text + ")";
+             comm = new OleDbCommand(sql, conn);
+
+             if (comm.ExecuteNonQuery() > 0)
+             {
+                 MessageBox.Show("New Book has been saved", "", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+             }
+             sql = "Select * from DataSudent";
+             DA = new OleDbDataAdapter(sql, conn);
+             ds.Tables.Clear();
+
+             DA.Fill(ds, "DataSudent");
+             dataGridView1.DataSource = ds.Tables[0].DefaultView;
+         }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            string sql;
+            sql = "Select MAX(SID)+1 from DataSudent";
+            comm = new OleDbCommand(sql, conn);
+            stuID.Text = comm.ExecuteScalar().ToString();
+            stuName.Text = "";
+            comboBox1.Text = "";
+            Gpa.Text = "";
+        }
+    }
+    
+}
